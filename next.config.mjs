f0aa46b4bc65114e64412ddb64ab0bda3c88e389
Froshboi <120038259+Froshboi@users.noreply.github.com>
2026-07-31 19:27:05 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // 🔑 REQUIRED for container deployments
   reactStrictMode: true,
   images: {
     remotePatterns: [
